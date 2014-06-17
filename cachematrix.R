0b96a3ad177ce2cf0db3d94538aadac39826daf8
jyothishnet <jyothishnet@gmail.com>
2014-06-18 00:11:27 +0530

    Added Basic  Matrix Caching functionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Contains functions for storing and Retrieving Inverse of Matrix
 
-## Write a short comment describing this function
+## Stores the matrix and Caches Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL;
+  set<-function(y) {
+    x<<-y;
+    inv <<- NULL;
+    
+  }
+  
+  get<-function() x;
+  
+  setCache <- function(invMatrix) {
+    inv <<- invMatrix;
+    
+  }
+  
+  getInverseMatrix <- function() inv;
+  
+  list(set=set,get=get,setCache=setCache,getInverse=getInverseMatrix)  
+  
 }
 
 
-## Write a short comment describing this function
-
+## Returns the Inverse of Matrix, Stores in Cache if not present already 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  invMatrix <- x$getInverse();
+  if(!is.null(invMatrix)) {
+    message("Retreiving from Cache");    
+    return(inv);
+  }
+  
+  orgMatrix <- x$get();
+  invMatrix <- solve(orgMatrix);
+  x$setCache(invMatrix);
+  invMatrix;
+  
 }
